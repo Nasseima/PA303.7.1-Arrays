75c37674114747e589928f3915369117d9f3669c
Nasseima <lindsaynasseima@gmail.com>
2024-07-08 09:37:41 -0400

    Getting the hang of creating arrays in java and using the array methods

---
+++ src/Task1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+      int[] arr = {1, 2, 3};
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int [] array = { 13,5,7,68,2};
+        System.out.println(array[2]);
+    }
+}
